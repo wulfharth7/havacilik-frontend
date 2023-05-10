5b7bd264bee389bfd54c54cb51c2ebd49df82aca
wulfharth7 <legendcrafter24@gmail.com>
2023-05-10 15:48:26 +0300

    carousel added to the homepage, the code is a mess right now and the carousel is made by css, not primeng or smht else

--- havacilik/havacilik/src/app/core/pages/homepage/homepage.component.ts
+++ havacilik/havacilik/src/app/core/pages/homepage/homepage.component.ts
@@ -1,10 +1,24 @@
 import { Component, ViewChild } from '@angular/core';
- 
+import {style, state, animate, transition, trigger} from '@angular/animations';
+
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
+  
 })
 export class HomepageComponent {
-  
+  carousel1: boolean=true;
+  carousel2: boolean=false;
+
+  div1Function(){
+    this.carousel1=true;
+    this.carousel2=false
+}
+
+  div2Function(){
+    this.carousel1=false;
+    this.carousel2=true
+  }
+
 }
