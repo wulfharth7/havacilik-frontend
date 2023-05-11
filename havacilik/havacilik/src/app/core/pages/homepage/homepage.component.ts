abbc4a7cb84a39c25931b49dae34a11d8de5f899
wulfharth7 <legendcrafter24@gmail.com>
2023-05-11 11:31:53 +0300

    improving the carousel

--- havacilik/havacilik/src/app/core/pages/homepage/homepage.component.ts
+++ havacilik/havacilik/src/app/core/pages/homepage/homepage.component.ts
@@ -8,17 +8,17 @@ import {style, state, animate, transition, trigger} from '@angular/animations';
   
 })
 export class HomepageComponent {
-  carousel1: boolean=true;
-  carousel2: boolean=false;
+  carousel1: boolean=false;
+  carousel2: boolean=true;
 
-  div1Function(){
-    this.carousel1=true;
-    this.carousel2=false
-}
-
-  div2Function(){
+  carousel1Button(){
     this.carousel1=false;
     this.carousel2=true
+}
+
+  carousel2Button(){
+    this.carousel1=true;
+    this.carousel2=false
   }
 
 }
